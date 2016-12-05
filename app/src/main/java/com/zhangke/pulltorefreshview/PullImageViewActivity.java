79ec63b96d87ab18f7a3551ac67df3315f5e5a1d
zhangke <kaifa1321@163.com>
2016-12-05 17:35:51 +0800

    modify

--- app/src/main/java/com/zhangke/pulltorefreshview/PullImageViewActivity.java
+++ app/src/main/java/com/zhangke/pulltorefreshview/PullImageViewActivity.java
@@ -16,7 +16,6 @@ import java.net.URL;
 
 public class PullImageViewActivity extends AppCompatActivity {
 
-
     private PullRefreshLayout mPullRefreshLayout;
     private PullImageView imageView;
 
@@ -38,27 +37,6 @@ public class PullImageViewActivity extends AppCompatActivity {
                 mPullRefreshLayout.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        int length = images.length;
-
-                        try {
-                            URL url = new URL(images[++count % length]);
-
-                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                            conn.setReadTimeout(10 * 1000);
-                            conn.setConnectTimeout(10 * 1000);
-                            conn.setRequestMethod("GET");
-                            InputStream inputStream = conn.getInputStream();
-                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                            BitmapDrawable drawable = new BitmapDrawable(bitmap);
-                            imageView.setImageDrawable(drawable);
-
-                            inputStream.close();
-
-                            mPullRefreshLayout.onComplete(true);
-                        } catch (Exception e) {
-
-                        }
-
 
 
                     }
@@ -71,24 +49,7 @@ public class PullImageViewActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
-    public static String[] images = {"http://img.my.csdn.net/uploads/201407/26/1406383299_1976.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383291_6518.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383291_8239.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383290_9329.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383290_1042.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383275_3977.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383265_8550.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383264_3954.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383264_4787.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383264_8243.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383248_3693.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383243_5120.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383242_3127.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383242_9576.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383242_1721.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383219_5806.jpg",};
 
 }
